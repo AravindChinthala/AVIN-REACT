67a3f5d6c7e755d2cf0677691d79350498c92e33
Aravind Chinthala <aravindchinthla@gmail.com>
2023-08-15 17:43:10 +0530

    all filr

--- 1. Inception/App.js
+++ 1. Inception/App.js
@@ -1,13 +1,82 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const Header = () =>{
+    return (
+        <div className="header">
+            <div className="logo">
+            <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact us</li>
+                    <li>Kart</li>
+                   
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        
+            <div className="app">
+
+            </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
+
+/*const heading = React.createElement(
+    "h1",
+    {id: "heading"},"Namaste React"
+);
+console.log(heading);*/
+
+//React Element
+//JSX
+/*const jsxHeading = (
+<h1 className="head" tabIndex="5">
+    Namaste React Using JSX</h1>)
+
+console.log(jsxHeading);
+
+
+
+// React Component
+
+//Normal function
+const Abc = function () {
+    return (<>
+    <h1>Namaste Functional component</h1>
+    </>)
+}
+//Arrow function
+const Def = () => {
+    return <>
+    <Abc />
+    <h1>Component composition- composing one component to other</h1>
+    </>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Def />);
+
 
 //ReactElement(object) => HTML(Browser Understands)
 
-const parent = React.createElement(
+/* const parent = React.createElement(
     "div",
      {id:"parent"}, 
      [React.createElement(
       "div",
        {id:"children"}, 
-      [React.createElement("h1", {}, "Im Nested HTML element"),
+      [React.createElement("h1", {}, "This is Namaste React"),
       React.createElement("h2", {}, "Im Nested HTML element2")]
       ),
       React.createElement(
@@ -25,4 +94,5 @@ console.log(parent)
 
         const root = ReactDOM.createRoot(document.getElementById("root"));
 
-           root.render(parent);
+           root.render(parent); */
+
---
+++ 1. Inception/readme.md
@@ -0,0 +1,22 @@
+# Namaste React 
+
+# parcel
+-Dev Build
+-Local server
+-HMR: Hot Module replacement(Automatically refreshes our page) by...
+- Parcel uses File watching Algorithm - written in C++
+-Faster Builds -> caching
+- Parcel will do image optimization
+-Minification
+-Bundling
+- Compresses our file
+- Consitent Hashing
+- Code splitting
+- Differential Bundling -> To support older browsers and diff type of bundles
+- Diagnostics
+- Error Handling
+- To host on HTTPs
+- Tree shaking - removes unused code
+
+
+- "main": "App.js", Have to remove this main */ 
